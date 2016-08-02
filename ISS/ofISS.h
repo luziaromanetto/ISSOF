0b4462eec028d004030d5aa316256f7eb5cebc03
luzia <luziaromanetto@gmail.com>
2016-08-02 14:37:17 -0300

    Change folder structure

--- ISS/ofISS.h
+++ ISS/ofISS.h
@@ -18,68 +18,71 @@ using namespace of;
 #define IPT_H
 
 class Point{
-	private:
-		int type;        // type==0 é um ponto de canto, 
-						 // type==1 é um ponto de centro.
-		double value;    // function value.
-	public:
-		Point(){
-			type = -1;
-			value = 0;
-		};
+    private:
+	int type;        // type==0 é um ponto de canto, 
+			 // type==1 é um ponto de centro.
+	double value;    // function value.
+    public:
+	Point(){
+	    type = -1;
+	    value = 0;
+	};
 		
-		Point(int t, double v){
-			type = t;
-			value = v;			
-		};
+	Point(int t, double v){
+	    type = t;
+	    value = v;			
+	};
 };
 
 #endif
-
 
 #ifndef ISS_H
 #define ISS_H
 
 class ofISS3D{
-	private:
-		int levels; 					// Number of degree levels
-		double **bbox,					// Bounding box for build the mesh
-				alpha_short, alpha_long,// Isosurface Stuffing parameters
-				dx;						// Minimal mesh size
-		ofMesh<ofDefault3D> *mesh;  	// Pointer to mesh
-		function<double(double*)> f;    // Level function
-		char output[50];
-		
-	public:
+
+    private:
+	int levels; 			// Number of degree levels
+	double **bbox,			// Bounding box for build the mesh
+		alpha_short, alpha_long,// Isosurface Stuffing parameters
+		dx;			// Minimal mesh size
+	ofMesh<ofDefault3D> *mesh;  	// Pointer to mesh
+	function<double(double*)> f;    // Level function
+	char output[50];
+
+    public:
 	
-		ofISS3D(ofMesh<ofDefault3D> *_mesh){
-			levels = 1;
-			bbox = new double*[2];
-			for( int i=0; i<3; i++){
-				bbox[i] = new double[3];
-			}
-			alpha_short = 0.35900;
-			alpha_long = 0.21509;
-			mesh = _mesh;
-		};
-		
-		bool readConfigurationFile(char fileName[]);
+    ofISS3D(ofMesh<ofDefault3D> *_mesh){
+	levels = 1;
+	bbox = new double*[2];
+	for( int i=0; i<3; i++){
+		bbox[i] = new double[3];
+	}
+	alpha_short = 0.35900;
+	alpha_long = 0.21509;
+	mesh = _mesh;
+    };
+    
+    bool readConfigurationFile(char fileName[]);
 };
 
 bool ofISS3D::readConfigurationFile(char fileName[]){
-	ifstream fin(fileName);
+    ifstream fin(fileName);
     string line;
     istringstream sin;
     string aux;
     
-    if (!fin) std::cerr << "Could not open the file!" << std::endl;
+    if (!fin){
+    	std::cerr << "Could not open the file!" << std::endl;
+	return false;
+    }
 
     string tag[7] = {"-xi ", "-xf ", "-le ", "-dx ", "-al ", "-as ", "-out "};
     bool tagFound[7] = {false,false,false,false,false,false,false};
 
     while (getline(fin, line) ) {
         sin.str(line.substr(line.find("=")+1));
-        //cout << line << endl;
+        cout << line << endl;
         if (line.find(tag[0]) != std::string::npos) {
             sin >> bbox[0][0] >> bbox[0][1] >> bbox[0][2];
             tagFound[0]=true;
@@ -115,10 +118,11 @@ bool ofISS3D::readConfigurationFile(char fileName[]){
     for( int i=0; i<7; i++){
         if( !tagFound[i] ){
             cout << "ERROR: tag \""<< tag[i] << "\" was not found!!" << endl;
+	    return false;
         }
     }
     
-	return true;
+    return true;
 };
 
 #endif
